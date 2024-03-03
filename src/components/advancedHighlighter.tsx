617c06543eaab80ec2aa6db2b12a29a1a15e0d3a
Amrit <amrit@cosys.com.np>
2024-03-03 11:41:59 +0545

    styling updated

--- src/components/advancedHighlighter.tsx
+++ src/components/advancedHighlighter.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import {
   Highlighter,
   useSelections,
-  SelectionType,
-} from "react-selection-highlighter";
-import { Toaster } from "react-hot-toast";
+  SelectionType
+} from 'react-selection-highlighter';
+import { Toaster } from 'react-hot-toast';
 
-import { text } from "../lib/text";
-import CustomPopover from "./customPopover";
+import { text } from '../lib/text';
+import CustomPopover from './customPopover';
 
 const AdvancedHighlighter = () => {
   const { setSelections } = useSelections();
 
   useEffect(() => {
-    const data = localStorage.getItem("selections");
+    const data = localStorage.getItem('selections');
     if (data) {
       const parsedSelections = JSON.parse(data) as SelectionType[];
       setSelections(parsedSelections);
@@ -23,11 +23,14 @@ const AdvancedHighlighter = () => {
   return (
     <div className="w-full  flex flex-col items-center justify-center">
       <Toaster />
-      <h2 className="text-xl font-extrabold p-10 text-center">
-        {" "}
-        Advanced example of react-selection-highlighter
+      <h2 className="text-xl font-extrabold p-5 text-center">
+        {' '}
+        Advanced example of React Selection Highlighter
       </h2>
-      <div className="w-7/12">
+      <p className="text-sm text-center">
+        Select text below to see component in action.
+      </p>
+      <div className="w-8/12 shadow-lg p-10 mb-20">
         <Highlighter
           className="tracking-wide  space-y-5 leading-6"
           selectionWrapperClassName=" bg-[#15F5BA] select-none relative group"
